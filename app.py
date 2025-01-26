a6ae9b9378cb1d81ecafbd5a25828a9193f131bb
Rida12-fatma <st125481@ait.asia>
2025-01-26 22:58:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ st.title("Text Generation with Pre-trained LSTM")
 
 # GitHub Repo URL for model and dataset
 github_model_url = 'https://github.com/St125050/nlpassignment2/blob/main/model.pth'  # Replace with your actual repo path
-github_dataset_url = 'https://github.com/St125050/nlpassignment2/blob/main/dataset.txt'  # Replace with your actual repo path
+github_dataset_url = 'https://github.com/Rida12-fatma/st125481_Rida-fatma-nlp-A2/blob/main/dataset.txt'  # Replace with your actual repo path
 
 # Local paths
 model_path = 'model.pth'
